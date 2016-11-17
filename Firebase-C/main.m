af957182ad7d792406dce960165b18ec448f4c94
Rui <rui@students-macbook-pro.local>
2016-11-17 13:16:38 -0500

    Initial Commit

---
+++ Firebase-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Firebase-C
+//
+//  Created by Rui on 2016-11-17.
+//  Copyright © 2016 Rui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
